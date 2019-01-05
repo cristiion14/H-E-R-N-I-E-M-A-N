fd8c66a7c5c627b3ae2e437f17247140fa0f40cd
cristiion14 <cristi.ion1996@gmail.com>
2019-01-05 18:14:51 +0000

    Flippy Knife Code

---
+++ Restart.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class Restart : MonoBehaviour {
+
+	
+     void OnTriggerEnter(Collider col)
+    {
+        if(col.tag == "Block")
+        {
+            Debug.Log("A atins rau");
+            RestartLevel();
+        }
+    }
+
+    void RestartLevel()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+}
---
+++ _GM.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class _GM : MonoBehaviour {
+
+    private GameObject ground;
+  
+	// Use this for initialization
+	void Start ()
+    {
+        //                                                      De terminat instantierea mapei
+        /*
+        ground = GameObject.Find("Ground");
+        for (int y = 0; y < 5; y++)
+        {
+            for (int x = 0; x < 5; x++)
+            {
+                Instantiate(ground, new Vector3(0, 0, x), Quaternion.identity);               
+            }
+        }
+        */
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+}
